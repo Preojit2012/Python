3ace54c8a41954e05c98d3320eabcd74bbdd6b90
Preojit2012 <preojit2012@gmail.com>
2026-08-23 15:27:27 +0600

    yes

---
+++ module1/lesson2/2.py
@@ -0,0 +1,9 @@
+age = 22
+city = "Tokyo"
+ID = 67
+is_student = True
+
+print(f"I am Preojit. My age is {age}.")
+print(f"I live in {city}.")
+print(f"My citizen ID is {ID}.")
+print(f"Am I student? -> {is_student}.")
