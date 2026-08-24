4e3a3e661e8ee54b1c14b2dfb13e9dfe28c5453e
Preojit2012 <preojit2012@gmail.com>
2026-08-24 15:49:12 +0600

    hi

---
+++ module1/lesson3/1and2.py
@@ -0,0 +1,17 @@
+text1="Python"
+text2="Programming"
+
+print(len(text1))
+
+print(text1.upper())
+
+print(text1.lower())
+
+print(text1[0:3])
+
+print(text1[::-1])
+
+print(text1.find("t"))
+
+text3 = text1 + " " + text2
+print(text3)
