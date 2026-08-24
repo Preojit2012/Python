3bac7a4e15d1b4d0eef2d19ecdb12bf37c5935c6
Preojit2012 <preojit2012@gmail.com>
2026-08-24 15:26:33 +0600

    hehe

---
+++ module1/lesson3/3.py
@@ -0,0 +1,14 @@
+age=22
+total_marks=99.25
+name="Akon"
+is_student=True
+
+
+print(type(age))
+print(type(total_marks))
+print(type(name))
+print(type(is_student))
+
+
+total_marks=int(total_marks)
+print(f"The boy who became first is {name} and he got {total_marks}.")
