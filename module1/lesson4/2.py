014c2dd0ea732571230c1fd2c5611eb9ea081dc8
Preojit2012 <preojit2012@gmail.com>
2026-08-27 13:50:25 +0600

    huh

---
+++ module1/lesson4/2.py
@@ -0,0 +1,11 @@
+tree1 = 91
+tree2 = 67
+tree3 = 14
+tree4 = 22
+tree5 = 94
+
+total_height= tree1+tree2+tree3+tree4+tree5
+print(f"Total height of the trees: {total_height}.")
+
+avg_height = total_height/5
+print(f"The average height of the 5 trees: {avg_height}.")
